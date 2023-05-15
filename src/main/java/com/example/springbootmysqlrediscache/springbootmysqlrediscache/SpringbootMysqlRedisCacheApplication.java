8db1d80124d87f1672b9bb1e2191bd49dca3e1d2
Monir <monir.bhuiyanbjitgroup.com>
2023-05-15 15:47:38 +0600

    initial commit

---
+++ src/main/java/com/example/springbootmysqlrediscache/springbootmysqlrediscache/SpringbootMysqlRedisCacheApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootmysqlrediscache.springbootmysqlrediscache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMysqlRedisCacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMysqlRedisCacheApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootmysqlrediscache/springbootmysqlrediscache/SpringbootMysqlRedisCacheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootmysqlrediscache.springbootmysqlrediscache;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMysqlRedisCacheApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
